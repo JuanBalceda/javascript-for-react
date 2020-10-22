fd8b17de94575ceb3fc1377caaeccbae6e5db7af
Juan Balceda <juanbalceda@gmail.com>
2020-10-22 02:17:38 +0200

    Added modules dynamic imports sample

---
+++ 02-modules/05-module-dynamic-imports/js/app.js
@@ -0,0 +1,10 @@
+video.addEventListener('click', function () {
+
+  this.classList.add('hidden')
+
+  import('./player.js').then(({ player }) => {
+    setTimeout(() => {
+      player.play()
+    }, 200)
+  })
+})
