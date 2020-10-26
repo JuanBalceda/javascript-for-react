30fe221c26b935384bb7f84d982207f5a52f4010
Juan Balceda <juanbalceda@gmail.com>
2020-10-26 23:40:41 +0100

    Added forEach sample

---
+++ 03-arrays/js/render.js
@@ -0,0 +1,31 @@
+function renderElement(movie) {
+  const element = buildElement(movie)
+
+  window.container.append(element)
+}
+
+function buildElement({ title, poster_path, vote_average, id }) {
+
+  const template = `
+  <article class="movie recommended">
+    <img class="movie-poster" src="//image.tmdb.org/t/p/w220_and_h330_face/${poster_path}" alt="">
+    <p class="movie-title">${title}</p>
+    <p class="movie-id">${id}</p>
+    <span class="movie-rate">${vote_average}</span>
+  </article>
+`
+  const movie = document.createElement('div')
+  movie.innerHTML = template
+
+  return movie.firstElementChild
+}
+
+function cleanMovies(){
+  window.container.innerHTML = ''
+}
+
+export default function renderMovies(list) {
+  // console.table(list, ['id', 'title', 'poster_path', 'vote_average'])
+  cleanMovies()
+  list.forEach(renderElement)
+}
