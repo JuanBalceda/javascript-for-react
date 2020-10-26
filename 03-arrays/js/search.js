4b2521a6de8bfe47e3c76602db8e87a261289de8
Juan Balceda <juanbalceda@gmail.com>
2020-10-27 00:30:01 +0100

    Added find sample

--- 03-arrays/js/search.js
+++ 03-arrays/js/search.js
@@ -3,15 +3,31 @@ import render from './render.js'
 
 const $form = window['search-form']
 
-$form.addEventListener('submit', function(event) {
+$form.addEventListener('submit', function (event) {
   event.preventDefault()
 
   const formData = new FormData(this)
-  const title = formData.get('title')
-  const movies = filterByTitle(title)
-  render(movies)
+  const query = formData.get('title')
+  const movies = searchMovie(query)
+  if (movies) {
+    return render(movies)
+  }
+
+  return alert('Movie not found')
 })
 
 function filterByTitle(title) {
   return movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase().trim()))
+}
+
+function filterById(id) {
+  return movies.find(movie => movie.id === parseInt(id, 10))
+}
+
+function searchMovie(query) {
+  if (isNaN(query)) {
+    return filterByTitle(query)
+  } else {
+    return [filterById(query)]
+  }
 }
